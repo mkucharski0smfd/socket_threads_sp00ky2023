d579ad07435eb56d65d2f3ba73199d01a23b7871
Michael Kucharski <55199579+michaelkuch-17@users.noreply.github.com>
2020-12-30 16:31:06 -0600

    Create client.py

---
+++ sockets/client.py
@@ -0,0 +1,45 @@
+import socket
+import threading
+
+HEADER = 64
+PORT = 5000
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = '127.0.1.1'
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+def send(msg):
+	message = msg.encode(FORMAT)
+	msg_length = len(message)
+	send_length = str(msg_length).encode(FORMAT)
+	send_length += b' ' * (HEADER - len(send_length))
+	client.send(send_length)
+	client.send(message)
+
+
+# client only receives what they send, need to fix
+def receiver():
+	while True:
+		print(client.recv(2048))
+
+
+def start(handle):
+	client.connect(ADDR)
+	send(handle)
+	while True:
+		thread = threading.Thread(target=receiver)
+		thread.start()
+		to_send = input('[MY MESSAGE]: ')
+		if to_send == 'd':
+			send(DISCONNECT_MESSAGE)
+			client.close()
+			quit()
+			break
+		send(to_send)
+
+print("[CONNECTING] Connecting to Server...")
+handle = input("Enter handle: ")
+start(handle)
